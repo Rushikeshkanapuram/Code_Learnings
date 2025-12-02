d3926e31d68830a0b6889d3ae00c2a43b5d2e4e5
Rushikeshkanapuram <kanapuramrushi@gmail.com>
2025-12-02 19:07:07 +0530

    Double Checked Lock implemented

--- LLD/Design Patterns/Singleton Design Pattern/Singleton.java
+++ LLD/Design Patterns/Singleton Design Pattern/Singleton.java
@@ -1,13 +1,33 @@
-public class Singleton {
+// public class Singleton {
     
-     // Lazy initialization can be optimized for multithreads
-    private static Singleton instance;  
+//      // Lazy initialization can be optimized for multithreads
+//     private static Singleton instance;  
 
-    private Singleton() { }
+//     private Singleton() { }
+
+//     public static Singleton getInstance() {
+//         if (instance == null) {
+//             instance = new Singleton();  
+//         }
+//         return instance;
+//     }
+// }
+
+
+
+// Double checked Locking 
+public class Singleton {
+    private static volatile Singleton instance;
+
+    private Singleton() {}
 
     public static Singleton getInstance() {
-        if (instance == null) {
-            instance = new Singleton();  
+        if (instance == null) {   
+            synchronized (Singleton.class) {  
+                if (instance == null) {     
+                    instance = new Singleton();
+                }
+            }
         }
         return instance;
     }
